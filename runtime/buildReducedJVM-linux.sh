1a4d61846d5f5ed4346a5bd92d7e4236d2d9ac2b
mos <mos@Yogi-Bear>
2019-10-27 09:54:56 +0100

    Updated Linux JVM to 11.05.

--- runtime/buildReducedJVM-linux.sh
+++ runtime/buildReducedJVM-linux.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/home/mos/java/jdk-11.0.3+7
+export JAVA_HOME=/home/mos/java/jdk-11.0.5+10
 export DESTINATION_FOLDER=linux/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.desktop,java.xml,java.naming,jdk.accessibility --output $DESTINATION_FOLDER
