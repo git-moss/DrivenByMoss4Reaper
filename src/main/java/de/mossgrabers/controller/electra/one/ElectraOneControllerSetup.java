f26b5806809be8e9420a9c72c9d53073fc57bd35
git-moss <juergen.mossgraber@googlemail.com>
2022-05-30 15:40:20 +0200

    First DBM 17 copy. Updated JDK to 17.0.3.

---
+++ src/main/java/de/mossgrabers/controller/electra/one/ElectraOneControllerSetup.java
@@ -0,0 +1,308 @@
+// Written by Jürgen Moßgraber - mossgrabers.de
+// (c) 2017-2022
+// Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
+
+package de.mossgrabers.controller.electra.one;
+
+import de.mossgrabers.controller.electra.one.controller.ElectraOneColorManager;
+import de.mossgrabers.controller.electra.one.controller.ElectraOneControlSurface;
+import de.mossgrabers.controller.electra.one.mode.MixerMode;
+import de.mossgrabers.controller.electra.one.mode.SendsMode;
+import de.mossgrabers.framework.command.trigger.mode.ButtonRowModeCommand;
+import de.mossgrabers.framework.configuration.ISettingsUI;
+import de.mossgrabers.framework.controller.AbstractControllerSetup;
+import de.mossgrabers.framework.controller.ButtonID;
+import de.mossgrabers.framework.controller.ContinuousID;
+import de.mossgrabers.framework.controller.ISetupFactory;
+import de.mossgrabers.framework.controller.hardware.BindType;
+import de.mossgrabers.framework.controller.hardware.IHwAbsoluteKnob;
+import de.mossgrabers.framework.controller.valuechanger.TwosComplementValueChanger;
+import de.mossgrabers.framework.daw.IHost;
+import de.mossgrabers.framework.daw.ITransport;
+import de.mossgrabers.framework.daw.ModelSetup;
+import de.mossgrabers.framework.daw.data.ICursorDevice;
+import de.mossgrabers.framework.daw.data.bank.ITrackBank;
+import de.mossgrabers.framework.daw.midi.IMidiAccess;
+import de.mossgrabers.framework.daw.midi.IMidiInput;
+import de.mossgrabers.framework.daw.midi.IMidiOutput;
+import de.mossgrabers.framework.featuregroup.ModeManager;
+import de.mossgrabers.framework.mode.Modes;
+import de.mossgrabers.framework.view.DummyView;
+import de.mossgrabers.framework.view.Views;
+
+
+/**
+ * Support for the Electra.One controller.
+ *
+ * @author J&uuml;rgen Mo&szlig;graber
+ */
+public class ElectraOneControllerSetup extends AbstractControllerSetup<ElectraOneControlSurface, ElectraOneConfiguration>
+{
+    /**
+     * Constructor.
+     *
+     * @param host The DAW host
+     * @param factory The factory
+     * @param globalSettings The global settings
+     * @param documentSettings The document (project) specific settings
+     */
+    public ElectraOneControllerSetup (final IHost host, final ISetupFactory factory, final ISettingsUI globalSettings, final ISettingsUI documentSettings)
+    {
+        super (factory, host, globalSettings, documentSettings);
+
+        this.colorManager = new ElectraOneColorManager ();
+        this.valueChanger = new TwosComplementValueChanger (128, 1);
+        this.configuration = new ElectraOneConfiguration (host, this.valueChanger, factory.getArpeggiatorModes ());
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public void flush ()
+    {
+        super.flush ();
+
+        // TODO
+        // this.surfaces.forEach (surface -> {
+        // final ModeManager modeManager = surface.getModeManager ();
+        // final Modes mode = modeManager.getActiveID ();
+        // this.updateMode (mode);
+        //
+        // if (mode == null)
+        // return;
+        //
+        // this.updateVUMeters ();
+        // this.updateFaders (surface.isShiftPressed ());
+        // this.updateSegmentDisplay ();
+        //
+        // final IMode activeOrTempMode = modeManager.getActive ();
+        // if (activeOrTempMode instanceof final BaseMode<?> baseMode)
+        // baseMode.updateKnobLEDs ();
+        // });
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void createModel ()
+    {
+        final ModelSetup ms = new ModelSetup ();
+
+        ms.enableDrumDevice (false);
+        // TODO ms.enableDevice (DeviceID.EQ);
+
+        ms.setNumTracks (5);
+
+        ms.setHasFlatTrackList (true);
+        ms.setHasFullFlatTrackList (true);
+        ms.setNumSends (6);
+
+        // TODO
+        ms.setNumScenes (8);
+        ms.setNumFilterColumnEntries (8);
+        ms.setNumResults (8);
+        ms.setNumParamPages (8);
+        ms.setNumParams (8);
+        ms.setNumDeviceLayers (0);
+        ms.setNumDrumPadLayers (0);
+        ms.setNumMarkers (8);
+
+        this.model = this.factory.createModel (this.configuration, this.colorManager, this.valueChanger, this.scales, ms);
+
+        final ITrackBank trackBank = this.model.getTrackBank ();
+        trackBank.setIndication (true);
+        trackBank.addSelectionObserver ( (index, isSelected) -> this.handleTrackChange (isSelected));
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void createSurface ()
+    {
+        final IMidiAccess midiAccess = this.factory.createMidiAccess ();
+
+        final IMidiOutput output = midiAccess.createOutput (0);
+        final IMidiInput input = midiAccess.createInput (0, null);
+        final IMidiOutput ctrlOutput = midiAccess.createOutput (1);
+        final IMidiInput ctrlInput = midiAccess.createInput (1, null);
+        final ElectraOneControlSurface surface = new ElectraOneControlSurface (this.host, this.colorManager, this.configuration, output, input, ctrlInput, ctrlOutput);
+        this.surfaces.add (surface);
+        // TODO surface.addTextDisplay (new ElectraOneSegmentDisplay (this.host, output));
+
+        surface.getModeManager ().setDefaultID (Modes.VOLUME);
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void createModes ()
+    {
+        final ElectraOneControlSurface surface = this.getSurface ();
+        final ModeManager modeManager = surface.getModeManager ();
+
+        modeManager.register (Modes.VOLUME, new MixerMode (surface, this.model));
+        modeManager.register (Modes.SEND, new SendsMode (surface, this.model));
+
+        // TODO
+        // modeManager.register (Modes.DEVICE_PARAMS, new DeviceParamsMode (surface, this.model));
+        // modeManager.register (Modes.EQ_DEVICE_PARAMS, new DeviceParamsMode ("Equalizer",
+        // this.model.getSpecificDevice (DeviceID.EQ), surface, this.model));
+        // modeManager.register (Modes.MARKERS, new MarkerMode (surface, this.model));
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void createViews ()
+    {
+        final ElectraOneControlSurface surface = this.getSurface ();
+        // We need this for triggering updateDispay in the modes
+        surface.getViewManager ().register (Views.CONTROL, new DummyView<> ("Control", surface, this.model));
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void createObservers ()
+    {
+        super.createObservers ();
+
+        this.configuration.registerDeactivatedItemsHandler (this.model);
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void registerTriggerCommands ()
+    {
+        final ITransport t = this.model.getTransport ();
+        final ICursorDevice cursorDevice = this.model.getCursorDevice ();
+
+        final ElectraOneControlSurface surface = this.getSurface ();
+
+        // Navigation
+
+        // this.addButton (surface, ButtonID.LOOP, "Loop", new ToggleLoopCommand<> (this.model,
+        // surface), 0, ElectraOneControlSurface.ELECTRA_ONE_REPEAT, t::isLoop);
+        // this.addButton (surface, ButtonID.STOP, "Stop", new StopCommand<> (this.model, surface),
+        // 0, ElectraOneControlSurface.ELECTRA_ONE_STOP, () -> !t.isPlaying ());
+        // this.addButton (surface, ButtonID.PLAY, "Play", new PlayCommand<> (this.model, surface),
+        // 0, ElectraOneControlSurface.ELECTRA_ONE_PLAY, t::isPlaying);
+        // this.addButton (surface, ButtonID.RECORD, "Record", new ElectraOneRecordCommand
+        // (this.model, surface), 0, ElectraOneControlSurface.ELECTRA_ONE_RECORD, () -> {
+        // final boolean isOn = this.isRecordShifted (surface) ? t.isLauncherOverdub () :
+        // t.isRecording ();
+        // return isOn ? ElectraOne_BUTTON_STATE_ON : ElectraOne_BUTTON_STATE_OFF;
+        // });
+        //
+        // this.addButton (surface, ButtonID.MOVE_TRACK_LEFT, "Left", new
+        // ElectraOneMoveTrackBankCommand (this.model, surface, true, true), 0,
+        // ElectraOneControlSurface.ELECTRA_ONE_TRACK_LEFT);
+        // this.addButton (surface, ButtonID.MOVE_TRACK_RIGHT, TAG_RIGHT, new
+        // ElectraOneMoveTrackBankCommand (this.model, surface, true, false), 0,
+        // ElectraOneControlSurface.ELECTRA_ONE_TRACK_RIGHT);
+
+        for (int i = 0; i < 6; i++)
+        {
+            final ButtonID row1ButtonID = ButtonID.get (ButtonID.ROW1_1, i);
+            final ButtonID row2ButtonID = ButtonID.get (ButtonID.ROW2_1, i);
+            final ButtonID row3ButtonID = ButtonID.get (ButtonID.ROW3_1, i);
+            final ButtonID row4ButtonID = ButtonID.get (ButtonID.ROW4_1, i);
+
+            final int labelIndex = i + 1;
+
+            this.addButton (surface, row1ButtonID, "Rec Arm " + labelIndex, new ButtonRowModeCommand<> (0, i, this.model, surface), ElectraOneControlSurface.ELECTRA_ONE_ARM1 + i, () -> getButtonColor (surface, row1ButtonID));
+            this.addButton (surface, row3ButtonID, "Mute " + labelIndex, new ButtonRowModeCommand<> (1, i, this.model, surface), ElectraOneControlSurface.ELECTRA_ONE_MUTE1 + i, () -> getButtonColor (surface, row2ButtonID));
+            this.addButton (surface, row2ButtonID, "Solo " + labelIndex, new ButtonRowModeCommand<> (2, i, this.model, surface), ElectraOneControlSurface.ELECTRA_ONE_SOLO1 + i, () -> getButtonColor (surface, row3ButtonID));
+            this.addButton (surface, row4ButtonID, "Select " + labelIndex, new ButtonRowModeCommand<> (3, i, this.model, surface), ElectraOneControlSurface.ELECTRA_ONE_SELECT1 + i, () -> getButtonColor (surface, row4ButtonID));
+        }
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void registerContinuousCommands ()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            // Mixer Mode
+            final IHwAbsoluteKnob volumeKnob = this.addAbsoluteKnob (ContinuousID.get (ContinuousID.VOLUME_KNOB1, i), "Volume " + i, null, ElectraOneControlSurface.ELECTRA_ONE_VOLUME1 + i);
+            volumeKnob.setIndexInGroup (i);
+            volumeKnob.disableTakeOver ();
+            final IHwAbsoluteKnob panKnob = this.addAbsoluteKnob (ContinuousID.get (ContinuousID.PAN_KNOB1, i), "Pan " + i, null, ElectraOneControlSurface.ELECTRA_ONE_PAN1 + i);
+            panKnob.setIndexInGroup (i);
+            panKnob.disableTakeOver ();
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            // Send Mode
+            final IHwAbsoluteKnob send1Knob = this.addAbsoluteKnob (ContinuousID.get (ContinuousID.SEND1_KNOB1, i), "Send 1 " + i, null, BindType.CC, 1, ElectraOneControlSurface.ELECTRA_ONE_SEND1 + i);
+            send1Knob.setIndexInGroup (i);
+            send1Knob.disableTakeOver ();
+            final IHwAbsoluteKnob send2Knob = this.addAbsoluteKnob (ContinuousID.get (ContinuousID.SEND2_KNOB1, i), "Send 2 " + i, null, BindType.CC, 1, ElectraOneControlSurface.ELECTRA_ONE_SEND2 + i);
+            send2Knob.setIndexInGroup (i);
+            send2Knob.disableTakeOver ();
+            // final IHwAbsoluteKnob send3Knob = this.addAbsoluteKnob (ContinuousID.get
+            // (ContinuousID.SEND3_KNOB1, i), "Send 3 " + i, null, BindType.CC, 1,
+            // ElectraOneControlSurface.ELECTRA_ONE_SEND3 + i);
+            // send3Knob.setIndexInGroup (i);
+            // send3Knob.disableTakeOver ();
+            // final IHwAbsoluteKnob send4Knob = this.addAbsoluteKnob (ContinuousID.get
+            // (ContinuousID.SEND4_KNOB1, i), "Send 4 " + i, null, BindType.CC, 1,
+            // ElectraOneControlSurface.ELECTRA_ONE_SEND4 + i);
+            // send4Knob.setIndexInGroup (i);
+            // send4Knob.disableTakeOver ();
+            // final IHwAbsoluteKnob send5Knob = this.addAbsoluteKnob (ContinuousID.get
+            // (ContinuousID.SEND5_KNOB1, i), "Send 5 " + i, null, BindType.CC, 1,
+            // ElectraOneControlSurface.ELECTRA_ONE_SEND5 + i);
+            // send5Knob.setIndexInGroup (i);
+            // send5Knob.disableTakeOver ();
+            // final IHwAbsoluteKnob send6Knob = this.addAbsoluteKnob (ContinuousID.get
+            // (ContinuousID.SEND6_KNOB1, i), "Send 6 " + i, null, BindType.CC, 1,
+            // ElectraOneControlSurface.ELECTRA_ONE_SEND6 + i);
+            // send6Knob.setIndexInGroup (i);
+            // send6Knob.disableTakeOver ();
+        }
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void layoutControls ()
+    {
+        // TODO
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public void startup ()
+    {
+        final ElectraOneControlSurface surface = this.getSurface ();
+
+        surface.getViewManager ().setActive (Views.CONTROL);
+        surface.getModeManager ().setActive (Modes.VOLUME);
+    }
+
+
+    /**
+     * Handle a track selection change.
+     *
+     * @param isSelected Has the track been selected?
+     */
+    private void handleTrackChange (final boolean isSelected)
+    {
+        if (!isSelected)
+            return;
+
+        final ModeManager modeManager = this.getSurface ().getModeManager ();
+        if (modeManager.isActive (Modes.MASTER) && !this.model.getMasterTrack ().isSelected ())
+        {
+            if (Modes.isTrackMode (modeManager.getPreviousID ()))
+                modeManager.restore ();
+            else
+                modeManager.setActive (Modes.TRACK);
+        }
+    }
+}
---
+++ src/main/java/de/mossgrabers/controller/electra/one/controller/ElectraOneControlSurface.java
@@ -0,0 +1,278 @@
+// Written by Jürgen Moßgraber - mossgrabers.de
+// (c) 2017-2022
+// Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
+
+package de.mossgrabers.controller.electra.one.controller;
+
+import de.mossgrabers.controller.electra.one.ElectraOneConfiguration;
+import de.mossgrabers.framework.controller.AbstractControlSurface;
+import de.mossgrabers.framework.controller.color.ColorEx;
+import de.mossgrabers.framework.controller.color.ColorManager;
+import de.mossgrabers.framework.daw.IHost;
+import de.mossgrabers.framework.daw.midi.DeviceInquiry;
+import de.mossgrabers.framework.daw.midi.IMidiInput;
+import de.mossgrabers.framework.daw.midi.IMidiOutput;
+import de.mossgrabers.framework.mode.Modes;
+import de.mossgrabers.framework.utils.StringUtils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+
+/**
+ * A control surface which supports the Electra.One controller.
+ *
+ * @author J&uuml;rgen Mo&szlig;graber
+ */
+@SuppressWarnings("javadoc")
+public class ElectraOneControlSurface extends AbstractControlSurface<ElectraOneConfiguration>
+{
+    public static final int       ELECTRA_ONE_VOLUME1         = 10;
+    public static final int       ELECTRA_ONE_MASTER_VOLUME   = 15;
+
+    public static final int       ELECTRA_ONE_PAN1            = 20;
+    public static final int       ELECTRA_ONE_PLAY_POSITION   = 25;
+
+    public static final int       ELECTRA_ONE_ARM1            = 30;
+    public static final int       ELECTRA_ONE_NEXT_TRACK_PAGE = 35;
+
+    public static final int       ELECTRA_ONE_MUTE1           = 40;
+    public static final int       ELECTRA_ONE_PREV_TRACK_PAGE = 45;
+
+    public static final int       ELECTRA_ONE_SOLO1           = 50;
+    public static final int       ELECTRA_ONE_RECORD          = 55;
+
+    public static final int       ELECTRA_ONE_SELECT1         = 60;
+    public static final int       ELECTRA_ONE_PLAY            = 65;
+
+    public static final int       ELECTRA_ONE_SEND1           = 10;
+    public static final int       ELECTRA_ONE_SEND2           = 20;
+    public static final int       ELECTRA_ONE_SEND3           = 30;
+    public static final int       ELECTRA_ONE_SEND4           = 40;
+    public static final int       ELECTRA_ONE_SEND5           = 50;
+    public static final int       ELECTRA_ONE_SEND6           = 60;
+    public static final int       ELECTRA_ONE_CUE_VOLUME      = 15;
+
+    // Sysex
+
+    private static final byte []  SYSEX_HDR                   =
+    {
+        (byte) 0xF0,
+        0x00,
+        0x21,
+        0x45
+    };
+
+    private static final byte []  SYSEX_UPDATE_ELEMENT        =
+    {
+        0x14,
+        0x07
+    };
+
+    private static final byte []  SYSEX_EXECUTE_LUA           =
+    {
+        0x08,
+        0x0D
+    };
+
+    private static final int []   SYSEX_LOGGING               =
+    {
+        0xF0,
+        0x00,
+        0x21,
+        0x45,
+        0x7F,
+        0x00
+    };
+
+    private static final String   LOG_PAGE_CHANGE             = "displayPage: page shown: page=";
+    private static final Modes [] MODES                       =
+    {
+        Modes.VOLUME,
+        Modes.SEND
+    };
+
+    private final IMidiInput      ctrlInput;
+    private final IMidiOutput     ctrlOutput;
+
+
+    /**
+     * Constructor.
+     *
+     * @param host The host
+     * @param colorManager The color manager
+     * @param configuration The configuration
+     * @param output The MIDI output
+     * @param input The MIDI input
+     * @param ctrlOutput The control MIDI output
+     * @param ctrlInput The control MIDI input
+     */
+    public ElectraOneControlSurface (final IHost host, final ColorManager colorManager, final ElectraOneConfiguration configuration, final IMidiOutput output, final IMidiInput input, final IMidiInput ctrlInput, final IMidiOutput ctrlOutput)
+    {
+        super (host, configuration, colorManager, output, input, null, 1000, 1000);
+
+        this.ctrlInput = ctrlInput;
+        this.ctrlOutput = ctrlOutput;
+
+        this.ctrlInput.setSysexCallback (this::handleSysEx);
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public void setTrigger (final int channel, final int cc, final int value)
+    {
+        this.output.sendCCEx (channel, cc, value);
+    }
+
+
+    /**
+     * Set the title of a group element.
+     * 
+     * @param groupID The element starting from 1, increasing from left to right, top to bottom
+     * @param cache The cache to use
+     * @param title The title to set
+     */
+    public void updateGroupTitle (final int groupID, final String [] cache, final String title)
+    {
+        if (title.equals (cache[groupID]))
+            return;
+        cache[groupID] = title;
+        this.sendLua ("setGroupTitle(" + groupID + ",\"" + title + "\")");
+    }
+
+
+    /**
+     * Sets name, color and visibility of an element on the Electra.One.
+     *
+     * @param controlID The element starting from 1, increasing from left to right, top to bottom
+     * @param cache The message is only send if the parameters are different from the previous call
+     * @param name The name to set
+     * @param color The color to set
+     * @param visibility The visibility to set
+     */
+    public void updateElement (final int controlID, final String [] cache, final String name, final ColorEx color, final Boolean visibility)
+    {
+        final ObjectMapper mapper = new ObjectMapper ();
+
+        final StringWriter writer = new StringWriter ();
+        try (final JsonGenerator generator = mapper.createGenerator (writer))
+        {
+            generator.writeStartObject ();
+            if (name != null)
+                generator.writeStringField ("name", name);
+            if (color != null)
+                generator.writeStringField ("color", StringUtils.formatColor (color));
+            if (visibility != null)
+                generator.writeBooleanField ("visible", visibility.booleanValue ());
+            generator.writeEndObject ();
+        }
+        catch (final IOException ex)
+        {
+            this.host.error (name, ex);
+            return;
+        }
+
+        final String json = writer.toString ();
+
+        if (json.equals (cache[controlID]))
+            return;
+        cache[controlID] = json;
+
+        byte [] command = new byte [4];
+        command[0] = SYSEX_UPDATE_ELEMENT[0];
+        command[1] = SYSEX_UPDATE_ELEMENT[1];
+        command[2] = (byte) (controlID & 0x7F);
+        command[3] = (byte) (controlID >> 7);
+        sendText (command, json);
+    }
+
+
+    /**
+     * Send LUA code for execution to the Electra.One.
+     *
+     * @param elementCache The message is only send if the code is different from the previous call
+     * @param code The code to send
+     */
+    private void sendLua (final String code)
+    {
+        this.sendText (SYSEX_EXECUTE_LUA, code);
+    }
+
+
+    /**
+     * Send a JSON or LUA string to the CTRL output.
+     *
+     * @param command The command bytes
+     * @param text The JSON to send
+     */
+    private void sendText (final byte [] command, final String text)
+    {
+        this.sendSysex (command, StringUtils.fixASCII (text).getBytes ());
+    }
+
+
+    /**
+     * Send a byte array to the CTRL output of the Electra.One.
+     *
+     * @param command The command bytes
+     * @param content The content bytes
+     */
+    private void sendSysex (final byte [] command, final byte [] content)
+    {
+        try (final ByteArrayOutputStream out = new ByteArrayOutputStream ())
+        {
+            out.write (SYSEX_HDR);
+            out.write (command);
+            out.write (content);
+            out.write ((byte) 0xF7);
+            this.ctrlOutput.sendSysex (out.toByteArray ());
+        }
+        catch (final IOException ex)
+        {
+            this.host.error ("Could not send JSON command.", ex);
+        }
+    }
+
+
+    /**
+     * Handle incoming system exclusive data.
+     *
+     * @param data The data
+     */
+    private void handleSysEx (final String data)
+    {
+        final int [] byteData = StringUtils.fromHexStr (data);
+        final DeviceInquiry deviceInquiry = new DeviceInquiry (byteData);
+        if (deviceInquiry.isValid ())
+        {
+            // TODO
+            return;
+        }
+
+        if (Arrays.compareUnsigned (SYSEX_LOGGING, 0, SYSEX_LOGGING.length, byteData, 0, SYSEX_LOGGING.length) == 0)
+        {
+            final String message = new String (byteData, SYSEX_LOGGING.length, byteData.length - SYSEX_LOGGING.length - 1);
+            if (this.configuration.isLogToConsoleEnabled ())
+                this.host.println (message);
+
+            // Bad hack for missing page change event, replace when it becomes available
+            if (message.startsWith (LOG_PAGE_CHANGE))
+            {
+                final String rest = message.substring (LOG_PAGE_CHANGE.length ());
+                final int pos = rest.indexOf (',');
+                if (pos > 0)
+                {
+                    final int page = Integer.parseInt (rest.substring (0, pos));
+                    if (page < MODES.length)
+                        this.getModeManager ().setActive (MODES[page]);
+                }
+            }
+        }
+    }
+}
